66ec24f1dedb823aa5fbb6947f3e7a6e7b265886
Leemalgum <withxia1215@naver.com>
2021-09-09 09:20:42 +0900

    9월8일

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+    <div>
+      <label for=""></label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for=""></label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for=""></label>
+      <input type="text" />
+    </div>
+    <button>로그인</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
